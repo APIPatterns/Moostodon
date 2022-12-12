0ab65c3f6c27d29efec3e5d5b327949eb12e5b2c
Darrel Miller <darrel.miller@microsoft.com>
2022-12-11 21:16:15 -0500

    Added a CredsHack.cs file without any values in it so the app will compile

---
+++ clients/csharp/CredsHack.cs
@@ -0,0 +1,12 @@
+// Do not store your credentials like this it is just for demo purposes
+
+namespace MoostodonConsole {
+
+    public class CredsHack {
+        public const string ClientId = "";
+        public const string ClientSecret = "";
+
+        public const string Username = "";
+
+    }
+}
